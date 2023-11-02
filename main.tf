14652e6b176c5fbba87ade2391a2203b56c58971
marfelizardo <mar.felizardo@gmail.com>
2023-11-02 00:08:33 +0000

    commit inicial

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b6c2d49148000cd5"
+  instance_type = "t2.micro"
+  key_name="iac-alura"
+
+tags = {
+    Name = "Primeira Instancia"
+  }
+}
+
