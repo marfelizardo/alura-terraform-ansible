0fa1cd4507f3bda13a7eeeecc14adbebd5a2ce7d
marfelizardo <mar.felizardo@gmail.com>
2023-11-02 22:43:57 +0000

    playbook ansible

--- main.tf
+++ main.tf
@@ -17,9 +17,15 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0b6c2d49148000cd5"
   instance_type = "t2.micro"
   key_name="iac-alura"
-
-tags = {
-    Name = "Primeira Instancia"
+#  user_data = <<-EOF
+#                #!/bin/bash
+#                cd /home/ubuntu
+#                echo "<h1>Mensagem a ser mostrada</h1>" > index.html
+#                nohup busybox httpd -f -p 8080 &
+#                EOF
+#  user_data_replace_on_change = true
+  tags = {
+    Name = "AWS terraform"
   }
 }
 
